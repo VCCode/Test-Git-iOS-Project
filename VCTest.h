ce95d9cb65dae02b9ffc8d571ae65fff18205953
Vanja Catak <vanjacatak@vanjas-imac.sx76x.gigaset.net>
2014-03-29 18:21:58 +0100

    added test class

---
+++ VCTest.h
@@ -0,0 +1,13 @@
+//
+//  VCTest.h
+//  Git Test
+//
+//  Created by Vanja Catak on 29/03/14.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface VCTest : NSObject
+
+@end
---
+++ VCTest.m
@@ -0,0 +1,13 @@
+//
+//  VCTest.m
+//  Git Test
+//
+//  Created by Vanja Catak on 29/03/14.
+//
+//
+
+#import "VCTest.h"
+
+@implementation VCTest
+
+@end
